1dfce8970d932113f742d4a715e449fdb50582a3
Noémie Grémeaux <noemie.gremeaux@ign.fr>
2023-04-19 17:34:44 +0200

    Correction syntaxe

--- functions/fr_shn_code.py
+++ functions/fr_shn_code.py
@@ -19,37 +19,35 @@
     shn_code = ""
     code_commune = code_insee[len(code_insee)-4:len(code_insee-1)]
 
-    match gcms_territoire:
-        case 'GLP':
-            code_commune = code_insee[len(code_insee)-3:len(code_insee-1)]
-            shn_code = "GP{0}{1}{2}{3}".format(code_region, code_departement, code_arrondissement, code_commune)
+    if gcms_territoire == 'GLP':
+        code_commune = code_insee[len(code_insee)-3:len(code_insee-1)]
+        shn_code = "GP{0}{1}{2}{3}".format(code_region, code_departement, code_arrondissement, code_commune)
         
-        case 'GUF':
-            code_commune = code_insee[len(code_insee)-3:len(code_insee-1)]
-            shn_code = "GF{0}{1}{2}{3}".format(code_region, code_departement, code_arrondissement, code_commune)
+    elif gcms_territoire == 'GUF':
+        code_commune = code_insee[len(code_insee)-3:len(code_insee-1)]
+        shn_code = "GF{0}{1}{2}{3}".format(code_region, code_departement, code_arrondissement, code_commune)
         
-        case 'H_T':
-            shn_code = ""
+    elif gcms_territoire == 'H_T':
+        shn_code = ""
         
-        case 'MTQ':
-            code_commune = code_insee[len(code_insee)-3:len(code_insee-1)]
-            shn_code = "MQ{0}{1}{2}{3}".format(code_region, code_departement, code_arrondissement, code_commune)
+    elif gcms_territoire == 'MTQ':
+        code_commune = code_insee[len(code_insee)-3:len(code_insee-1)]
+        shn_code = "MQ{0}{1}{2}{3}".format(code_region, code_departement, code_arrondissement, code_commune)
         
-        case 'MYT':
-            code_commune = code_insee[len(code_insee)-3:len(code_insee-1)]
-            shn_code = "YT{0}{1}{2}{3}".format(code_region, code_departement, code_arrondissement, code_commune)
+    elif gcms_territoire == 'MYT':
+        code_commune = code_insee[len(code_insee)-3:len(code_insee-1)]
+        shn_code = "YT{0}{1}{2}{3}".format(code_region, code_departement, code_arrondissement, code_commune)
 
-        case 'REU':
-            code_commune = code_insee[len(code_insee)-3:len(code_insee-1)]
-            shn_code = "RE{0}{1}{2}{3}".format(code_region, code_departement, code_arrondissement, code_commune)
+    elif gcms_territoire == 'REU':
+        code_commune = code_insee[len(code_insee)-3:len(code_insee-1)]
+        shn_code = "RE{0}{1}{2}{3}".format(code_region, code_departement, code_arrondissement, code_commune)
 
-        case 'SPM':
-            shn_code = "PM{0}{1}{2}{3}".format(code_insee)
+    elif gcms_territoire == 'SPM':
+        shn_code = "PM{0}{1}{2}{3}".format(code_insee)
 
-        case _: # Cas général métropole (hors Paris, Marseille, Lyon)
-            code_commune = code_insee[len(code_insee)-4:len(code_insee-1)]
-            shn_code = "FR{0}{1}{2}{3}".format(code_region, code_departement, code_arrondissement, code_commune)
-
+    else: # Cas général métropole (hors Paris, Marseille, Lyon)
+        code_commune = code_insee[len(code_insee)-4:len(code_insee-1)]
+        shn_code = "FR{0}{1}{2}{3}".format(code_region, code_departement, code_arrondissement, code_commune)
 
     return shn_code
 
