1ff507b95bdb492c96954083a621aaccc279747f
Noémie Grémeaux <noemie.gremeaux@ign.fr>
2023-05-15 15:38:21 +0200

    Amélioration changement de modele fr-au

--- functions/fr_shn_code.py
+++ functions/fr_shn_code.py
@@ -1,35 +1,42 @@
 # def function_name(context):
 
     code_insee = context['data']['code_insee']
-    if code_insee == "" :
+    if code_insee == "" or code_insee == None :
         code_insee = "0"
 
+    code_epci = context['data']['code_epci_ebm']
+    if code_epci == "" or code_epci == None:
+        code_epci = "000"
+    
+    code_departement = context['data']['code_insee_du_departement']
+    if code_departement == "" or code_departement == None:
+        if gcms_territoire == "FXX":
+            code_departement = "00"
+        else:
+            code_departement = "000"
+
     code_arrondissement = context['data']['code_insee_de_l_arrondissement']
-    if code_arrondissement == "" :
+    if code_arrondissement == "" or code_arrondissement == None :
         code_arrondissement = "0"
 
-    code_departement = context['data']['code_insee_du_departement']
-    if code_departement == "":
-        code_departement = "0"
-
     code_region = context['data']['code_insee_de_la_region']  
-    if code_region == "":
-        code_region = "0"
+    if code_region == "" or code_region == None:
+        code_region = "00"
 
     gcms_territoire = context['data']['gcms_territoire']  
 
     shn_code = ""
-    code_commune = "0"
+    code_commune = "00"
 
     if gcms_territoire == 'GLP':
         if len(code_insee) >= 2:
             code_commune = code_insee[len(code_insee)-3:len(code_insee)-1]
-        shn_code = "GP{0}{1}{2}{3}".format(code_region, code_departement, code_arrondissement, code_commune)
+        shn_code = "GP{0}{1}{2}{3}{4}".format(code_region, code_departement, code_arrondissement, code_epci, code_commune)
         
     elif gcms_territoire == 'GUF':
         if len(code_insee) >= 2:
             code_commune = code_insee[len(code_insee)-3:len(code_insee)-1]
-        shn_code = "GF{0}{1}{2}{3}".format(code_region, code_departement, code_arrondissement, code_commune)
+        shn_code = "GF{0}{1}{2}{3}{4}".format(code_region, code_departement, code_arrondissement, code_epci, code_commune)
         
     elif gcms_territoire == 'H_T':
         shn_code = ""
@@ -37,25 +44,27 @@
     elif gcms_territoire == 'MTQ':
         if len(code_insee) >= 2:
             code_commune = code_insee[len(code_insee)-3:len(code_insee)-1]
-        shn_code = "MQ{0}{1}{2}{3}".format(code_region, code_departement, code_arrondissement, code_commune)
+        shn_code = "MQ{0}{1}{2}{3}{4}".format(code_region, code_departement, code_arrondissement, code_epci, code_commune)
         
-    elif gcms_territoire == 'MYT':
+    elif gcms_territoire == 'MYT': # Cas particulier : pas d'arrondissements
         if len(code_insee) >= 2:
             code_commune = code_insee[len(code_insee)-3:len(code_insee)-1]
-        shn_code = "YT{0}{1}{2}{3}".format(code_region, code_departement, code_arrondissement, code_commune)
+        shn_code = "YT{0}{1}0{2}{3}".format(code_region, code_departement, code_epci, code_commune)
 
     elif gcms_territoire == 'REU':
         if len(code_insee) >= 2:
             code_commune = code_insee[len(code_insee)-3:len(code_insee)-1]
-        shn_code = "RE{0}{1}{2}{3}".format(code_region, code_departement, code_arrondissement, code_commune)
+        shn_code = "RE{0}{1}{2}{3}{4}".format(code_region, code_departement, code_arrondissement, code_epci, code_commune)
 
     elif gcms_territoire == 'SPM':
         shn_code = "PM{0}".format(code_insee)
 
-    else: # Cas général métropole (hors Paris, Marseille, Lyon)
+    else: # Cas général métropole
         if len(code_insee) >= 3:
             code_commune = code_insee[len(code_insee)-4:len(code_insee)-1]
-        shn_code = "FR{0}{1}{2}{3}".format(code_region, code_departement, code_arrondissement, code_commune)
+        else:
+            code_commune = "000"
+        shn_code = "FR{0}{1}{2}{3}{4}".format(code_region, code_departement, code_arrondissement, code_epci, code_commune)
 
     return shn_code
 
