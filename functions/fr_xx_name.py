e65d7792ca79c83618917d723cdc5c0103553e08
Noémie Grémeaux <noemie.gremeaux@ign.fr>
2023-10-16 08:39:54 +0200

    Ajustements TRANS

--- functions/fr_xx_name.py
+++ functions/fr_xx_name.py
@@ -2,6 +2,8 @@
 
     if 'toponyme' in context['data']:
         nom = context['data']['toponyme']
+    if 'cpx_toponyme_route_nommee' in context['data']:
+        nom = context['data']['cpx_toponyme_route_nommee']
     elif 'cpx_toponyme_de_cours_d_eau' in context['data']:
         nom = context['data']['cpx_toponyme_de_cours_d_eau']
     elif 'cpx_toponyme_de_plan_d_eau' in context['data']:
