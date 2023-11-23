4a579a551c7ca02535551dfda807e27c36da70f6
Noémie Grémeaux <noemie.gremeaux@ign.fr>
2023-11-23 16:09:03 +0100

    Suppression des double quotes dans les noms NL (refusés par JSON)

--- functions/nl_xx_name.py
+++ functions/nl_xx_name.py
@@ -20,68 +20,68 @@
         fries_name = ""
     name_list = []
     
-    if official_name != "" :
+    if official_name != "" and official_name is not None:
         if official_name == nl_name:
             name_list.append({
-                "spelling": official_name,
+                "spelling": official_name.replace('"', ''),
                 "language": "dut",
                 "script": "latn",
                 "name_status": "official",
                 "nativeness": "endonym",
-                "spelling_latn": official_name,
+                "spelling_latn": official_name.replace('"', ''),
                 "display": 1
             })
-            if fries_name != "":
+            if fries_name != "" and fries_name is not None:
                 name_list.append({
-                "spelling": fries_name,
+                "spelling": fries_name.replace('"', ''),
                 "language": "fry",
                 "script": "latn",
                 "name_status": "official",
                 "nativeness": "endonym",
-                "spelling_latn": fries_name,
+                "spelling_latn": fries_name.replace('"', ''),
                 "display": 0
             })
                 
         else:
             name_list.append({
-                "spelling": official_name,
+                "spelling": official_name.replace('"', ''),
                 "language": "fry",
                 "script": "latn",
                 "name_status": "official",
                 "nativeness": "endonym",
-                "spelling_latn": official_name,
+                "spelling_latn": official_name.replace('"', ''),
                 "display": 1
             })
-            if nl_name != "":
+            if nl_name != "" and nl_name is not None:
                 name_list.append({
-                "spelling": nl_name,
+                "spelling": nl_name.replace('"', ''),
                 "language": "dut",
                 "script": "latn",
                 "name_status": "official",
                 "nativeness": "endonym",
-                "spelling_latn": nl_name,
+                "spelling_latn": nl_name.replace('"', ''),
                 "display": 0
             })
     
     else:
-        if nl_name != "":
+        if nl_name != "" and nl_name is not None:
             name_list.append({
-                "spelling": nl_name,
+                "spelling": nl_name.replace('"', ''),
                 "language": "dut",
                 "script": "latin",
                 "name_status": "official",
                 "nativeness": "endonym",
-                "spelling_latn": nl_name,
+                "spelling_latn": nl_name.replace('"', ''),
                 "display": 1
             })
-        if fries_name != "":
+        if fries_name != "" and fries_name is not None:
             name_list.append({
-                "spelling": fries_name,
+                "spelling": fries_name.replace('"', ''),
                 "language": "fry",
                 "script": "latin",
                 "name_status": "official",
                 "nativeness": "endonym",
-                "spelling_latn": fries_name,
+                "spelling_latn": fries_name.replace('"', ''),
                 "display": 2
             })
 
