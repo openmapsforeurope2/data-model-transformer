a6d05a1ef2da9382fcdcb78b55fc95d094f2f8b0
Noémie Grémeaux <noemie.gremeaux@ign.fr>
2024-04-19 11:38:59 +0200

    Corrections NL hydro suite aux retours du 17/04/2024

--- functions/ch_au_shn_code_level3.py
+++ functions/ch_au_shn_code_level3.py
@@ -7,7 +7,7 @@
     if len(bezirks_code) == 3:
         bezirks_code = "0{0}".format(bezirks_code)
 
-    shn_code = "CH{0}00".format(bezirks_code)
+    shn_code = "CH{0}0000".format(bezirks_code)
 
     return shn_code
 
