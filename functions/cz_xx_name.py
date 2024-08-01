e8d0e474707df256f32305f577446c996cb2e985
Noémie Grémeaux <noemie.gremeaux@ign.fr>
2024-08-01 11:15:29 +0200

    Tests noms + correction CRS CZ

--- functions/cz_xx_name.py
+++ functions/cz_xx_name.py
@@ -5,6 +5,8 @@
     else:
         name = ""
 
+    print ("name1 = " + name)
+
     if 'language' in context['data']:
         language = context['data']['language']
     else:
@@ -15,6 +17,7 @@
     if name != "" and name is not None :
         name = name.replace('\"', '')
         name = name.replace('"', '')
+        print ("name2 = " + name)
         name_list.append({
             "spelling": name,
             "language": language,
@@ -24,5 +27,7 @@
             "spelling_latn": name,
             "display": 1
         })
+    
+    print (name_list)
     
     return name_list
