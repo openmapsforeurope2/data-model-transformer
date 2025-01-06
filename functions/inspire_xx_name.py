28343252d013d08ba5b61df7657379c7fb7c94e7
Noémie Grémeaux <noemie.gremeaux@ign.fr>
2025-01-06 17:47:07 +0100

    Addition of dk-au

---
+++ functions/inspire_xx_name.py
@@ -0,0 +1,53 @@
+# def function_name(context):
+
+    spelling = ""
+    language = "void_unk"
+    script = "latn"
+    name_status = "void_unk"
+    nativeness = "endonym"
+    spelling_latn = ""
+    display = 1
+
+    if 'geographicalname_spelling_text' in context['data']:
+        spelling = context['data']['geographicalname_spelling_text']
+        spelling_latn = spelling
+    elif 'name_gn_spell_spellofna_text' in context['data']:
+        spelling = context['data']['name_gn_spell_spellofna_text']
+        spelling_latn = spelling
+
+    if 'geographicalname_spelling_script' in context['data']:
+        script = context['data']['geographicalname_spelling_script']
+    elif 'name_gn_spell_spellofna_script' in context['data']:
+        script = context['data']['name_gn_spell_spellofna_script']
+
+    if 'geographicalname_nativeness' in context['data']:
+        nativeness = context['data']['geographicalname_nativeness']
+    elif 'name_gn_nativeness' in context['data']:
+        nativeness = context['data']['name_gn_nativeness']
+    
+    if 'geographicalname_namestatus' in context['data']:
+        name_status = context['data']['geographicalname_namestatus']
+    elif 'name_gn_namestatus' in context['data']:
+        name_status = context['data']['name_gn_namestatus']
+
+    if 'geographicalname_language' in context['data']:
+        language = context['data']['geographicalname_language']
+    elif 'name_gn_language' in context['data']:
+        language = context['data']['name_gn_language']
+
+    name_list = []
+    
+    if spelling != "" and spelling is not None :  
+        spelling = spelling.replace('\"', '')
+        spelling = spelling.replace('"', '')    
+        name_list.append({
+            "spelling": spelling,
+            "language": language,
+            "script": script,
+            "name_status": name_status,
+            "nativeness": nativeness,
+            "spelling_latn": spelling_latn,
+            "display": display
+        })
+           
+    return name_list
