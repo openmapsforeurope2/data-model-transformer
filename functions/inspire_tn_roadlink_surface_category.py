f8e30afacd73948d90e4f5a1042af0a4cc4d2d80
Noémie Grémeaux <noemie.gremeaux@ign.fr>
2025-01-10 17:17:26 +0100

    Update case for inspire_tn_roadlink_surface_category.py

--- functions/inspire_tn_roadlink_surface_category.py
+++ functions/inspire_tn_roadlink_surface_category.py
@@ -4,7 +4,11 @@
 
     # Retrieve source formofway value
     if 'ome2_road_surface_category_surfacecategory' in context['data']:
-        surface_category = str.lower(context['data']['ome2_road_surface_category_surfacecategory'])
+        surface_category = context['data']['ome2_road_surface_category_surfacecategory']
+        if surface_category != '' and surface_category is not None:
+            surface_category = str.lower(surface_category)
+        else:
+            return "void_unk"
 
     # If formofway is provided as a link, we keep only the final part
     if surface_category.startswith("http"): 
