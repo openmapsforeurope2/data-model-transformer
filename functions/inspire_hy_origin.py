153495d2a697597bcf7a0076167dc29d1a106813
Noémie Grémeaux <noemie.gremeaux@ign.fr>
2025-04-16 11:19:31 +0200

    Add model conversion file for Luxembourg - HY theme and relevant function

---
+++ functions/inspire_hy_origin.py
@@ -0,0 +1,28 @@
+# def function_name(context):
+    
+    origin = ""
+
+    if 'origin' in context['data']:
+        origin = context['data']['origin']
+
+    if origin is None or origin == '':
+        return "void_unk"
+    else:
+        origin = str.lower(origin) 
+
+    # If a link is provided in the attribute, we keep the last part only
+    if origin.startswith("http"): 
+        pos = str.rfind(origin, "/")
+        if pos == -1:
+            pos = str.rfind(origin, "\\")
+        if pos != -1:                                   
+            origin = str.lower(origin[pos+1:])
+
+    # Other cases
+    if origin == "manmade":
+        return "man_made"
+
+    if origin == "natural":
+        return origin
+
+    return "void_unk"
