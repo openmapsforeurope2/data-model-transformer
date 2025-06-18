ebce56a2ec3c4c4b8ec4c23d8dc7bc68a7800fc6
Noémie Grémeaux <noemie.gremeaux@ign.fr>
2025-06-18 09:07:47 +0200

    Correction on railway_line_label

--- functions/es_tn_railwaylink_railway_line_label.py
+++ functions/es_tn_railwaylink_railway_line_label.py
@@ -19,6 +19,7 @@
         
     if railway_line_label != "":
         railway_line_label = railway_line_label[0:len(railway_line_label)-1]
+        railway_line_label = railway_line_label[0:255]
         return railway_line_label
     
     return 'void_unk'
