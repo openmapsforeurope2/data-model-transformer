62c13737e9c7fcc499b806d9a5f966ef09b60deb
Noémie Grémeaux <noemie.gremeaux@ign.fr>
2025-08-29 17:26:44 +0200

    Correction following comments from data producers: road names and labels should be filled using nombre, not nombre_alt. The query used is based on the mapping table (tipo_vial == 1000 and titular != -997)

--- functions/es_tn_road_label.py
+++ functions/es_tn_road_label.py
@@ -1,13 +1,21 @@
 # def function_name(context):
 
     ome2_vial = context['data']['ome2_vial']
+    titular = context['data']['titular']
+
+    # Pattern for road numbers : 1 or 2 letters, hyphen, then digits
+    pattern = r'^[A-Z]{1,2}-\d+$'
 
     label = ""
 
     for vial in ome2_vial:
-        name = vial['nombre_alt']
+        name = vial['nombre']
+        tipo_vial = vial['tipo_vial']
 
-        if name != "" and name is not None and name != '-997' and name != '-998':
+        if name is None or name == "" or name == '-997' or name == '-998' or tipo_vial is None or titular is None:
+            continue
+
+        if (tipo_vial == 1000 and titular != -997) and not re.match(pattern, name):
             name = name.replace('\"', '')
             name = name.replace('"', '')
             label += name + "#"
--- functions/es_tn_road_name.py
+++ functions/es_tn_road_name.py
@@ -3,6 +3,10 @@
     # Retrieve name value
 
     ome2_vial = context['data']['ome2_vial']
+    titular = context['data']['titular']
+
+    # Pattern for road numbers : 1 or 2 letters, hyphen, then digits
+    pattern = r'^[A-Z]{1,2}-\d+$'
 
     name = ""
     name_list = []
@@ -10,19 +14,23 @@
     # Name as JSON
 
     for vial in ome2_vial:
-        name = vial['nombre_alt']
+        name = vial['nombre']
+        tipo_vial = vial['tipo_vial']
 
-        if name != "" and name is not None and name != "-997" and name != '-998':
+        if name is None or name == "" or name == '-997' or name == '-998' or tipo_vial is None or titular is None:
+            continue
+
+        if (tipo_vial == 1000 and titular != -997) and not re.match(pattern, name):
             name = name.replace('\"', '')
             name = name.replace('"', '')
             name_list.append({
                 "spelling": name,
                 "language": "spa",
                 "script": "latn",
-                "name_status": "void_unk",
-                "nativeness": "void_unk",
+                "name_status": "official",
+                "nativeness": "endonym",
                 "spelling_latn": name,
                 "display": 1
             })
-
+    
     return name_list
