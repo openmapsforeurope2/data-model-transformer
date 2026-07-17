ea7f59c761e237ca9b2d33995374ae45e55a0454
Noémie Grémeaux <noemie.gremeaux@ign.fr>
2026-07-17 15:16:04 +0200

    Correct nativeness and name_status for cz - cf. https://github.com/openmapsforeurope2/ome2-production/issues/97

--- functions/inspire_xx_name.py
+++ functions/inspire_xx_name.py
@@ -11,26 +11,25 @@
 
     country = 'void_unk' if 'country' not in context['data'] or context['data'] == '' or context['data'] is None else context['data']['country']
 
+    ## spelling and spelling_latn
     if 'geographicalname_spelling_text' in context['data']:
         spelling = context['data']['geographicalname_spelling_text']
-        spelling_latn = spelling
     elif 'name_gn_spell_spellofna_text' in context['data']:
         spelling = context['data']['name_gn_spell_spellofna_text']
-        spelling_latn = spelling
     elif 'geographicalname' in context['data']:
         spelling = context['data']['geographicalname']
-        spelling_latn = spelling
     elif 'text' in context['data']:
         spelling = context['data']['text']
-        spelling_latn = spelling
     elif 'ome2_watercourse_geographicalname' in context['data']:
         spelling = context['data']['ome2_watercourse_geographicalname']
-        spelling_latn = spelling
     elif 'streetname' in context['data']:
-        name = context['data']['streetname']
+        spelling = context['data']['streetname']
     elif 'spellingofname_text' in context['data']:
-        name = context['data']['spellingofname_text']
+        spelling = context['data']['spellingofname_text']
 
+    spelling_latn = spelling
+
+    ## script
     if 'geographicalname_spelling_script' in context['data']:
         script = context['data']['geographicalname_spelling_script']
     elif 'name_gn_spell_spellofna_script' in context['data']:
@@ -38,6 +37,7 @@
     elif 'script' in context['data']:
         script = context['data']['script']
 
+    ## nativeness
     if 'geographicalname_nativeness' in context['data']:
         nativeness = context['data']['geographicalname_nativeness']
     elif 'name_gn_nativeness' in context['data']:
@@ -45,6 +45,14 @@
     elif 'nativeness' in context['data']:
         nativeness = context['data']['nativeness']
     
+    if nativeness != "" and nativeness is not None and nativeness.startswith("http"): 
+        pos = str.rfind(nativeness, "/")
+        if pos == -1:
+            pos = str.rfind(nativeness, "\\")
+        if pos != -1:                                   
+            nativeness = str.lower(nativeness[pos+1:])
+
+    ## name_status
     if 'geographicalname_namestatus' in context['data']:
         name_status = context['data']['geographicalname_namestatus']
     elif 'name_gn_namestatus' in context['data']:
@@ -52,6 +60,14 @@
     elif 'namestatus' in context['data']:
         name_status = context['data']['namestatus']
 
+    if name_status != "" and name_status is not None and name_status.startswith("http"): 
+        pos = str.rfind(name_status, "/")
+        if pos == -1:
+            pos = str.rfind(name_status, "\\")
+        if pos != -1:                                   
+            name_status = str.lower(name_status[pos+1:])
+
+    ## language
     if 'geographicalname_language' in context['data']:
         language = context['data']['geographicalname_language']
     elif 'name_gn_language' in context['data']:
